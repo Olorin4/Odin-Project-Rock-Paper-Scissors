988b41dc6ff3871706d60fa25e8f5f1509009bb4
Nick Kalas <nick.kalas@proton.me>
2024-03-24 23:32:38 +0200

    Add function for player selection

--- script.js
+++ script.js
@@ -18,15 +18,30 @@ function getComputerChoice() {
             computerSelection = 'Scissors';
             console.log("The computer selected: " + computerSelection + "!");
             break;
+    return computerSelection;
     }
 }
 
+let playerAnswer = prompt("Please select: Rock, Paper or Scissor?");
+// Convert user's answer to a lower case string with an upper case first letter 
+let PlayerSelection = playerAnswer.charAt(0).toUpperCase() + playerAnswer.slice(1).toLowerCase();
+console.log("You selected: " + PlayerSelection + "!");
+
 getComputerChoice();
 
-let playerSelection = prompt("Please select: Rock, Paper or Scissor?");
-let modedPlayerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
-console.log("You selected: " + modedPlayerSelection + "!");
+function playRound(PlayerSelection, computerSelection) {
+    let result1;
+    switch (result1) {
+        case PlayerSelection === computerSelection:
+            console.log("It's a tie!");
+            break; 
+        case PlayerSelection === 'Rock' && computerSelection === 'Paper':
+            console.log("You lost! Paper beats Rock.");
+            break;
+    }
+}
 
+playRound(PlayerSelection, computerSelection);
 
 
 
