c5f55f916692920639d8bd4c7b1744ed0092503a
Nick Kalas <nick.kalas@proton.me>
2024-03-30 23:29:04 +0200

    Troubleshooting the correct chaining of functions

--- script.js
+++ script.js
@@ -1,133 +1,120 @@
 let computerSelection;
-
-function getComputerChoice() {
-    // Generate a random number between 0 and 2
-    const randomNumber = Math.floor(Math.random() * 3);
-    
-    // Assign 'Rock', 'Paper', or 'Scissors' based on the random number
-    switch (randomNumber) {
+function getComputerSelection() {
+    const randomNumber = Math.floor(Math.random() * 3);    // Generate a random number between 0 and 2
+    switch (randomNumber) {                       // Assign "Rock", "Paper", or "Scissors" based on the random number
         case 0:
-            computerSelection = 'rock';
+            computerSelection = "rock";
             break;
         case 1:
-            computerSelection = 'paper';
+            computerSelection = "paper";
             break;
         case 2:
-            computerSelection = 'scissors';
+            computerSelection = "scissors";
             break;
     }
     return computerSelection;
 }
 
-//Add div for displaying results
-const container = document.querySelector(".container");
-const div = document.createElement("div");
+  //Add div for displaying the results
+let container = document.querySelector(".container");
+let div = document.createElement("div"); 
 div.classList.add("results");
 container.appendChild(div);
+let para;
 
-let playerAnswer;
-let playerSelection;
-let resultMessage;
+// Triggers div creation and computer selection
+function getPlayerSelection() {
+    getComputerSelection();
+    para = document.createElement("p");
+    div.appendChild(para);
+    para.textContent = ("You selected: " + playerSelection + "! ");
+    para.textContent += (" The computer selected: " + computerSelection + "!");
+}
 
 // button events
+let playerSelection;
 let btn1 = document.querySelector("#rock");
 btn1.addEventListener("click", () => {
     playerSelection = "rock";
-    handlePlayerSelection();    
+    getPlayerSelection(); 
+    gameScore();
 });
 let btn2 = document.querySelector("#paper");
 btn2.addEventListener("click", () => {
     playerSelection = "paper";
-    handlePlayerSelection();
+    getPlayerSelection();
+    gameScore();
 });
 let btn3 = document.querySelector("#scissors");
 btn3.addEventListener("click", () => {
     playerSelection = "scissors";
-    handlePlayerSelection();
+    getPlayerSelection();
+    gameScore();
 });
 
-function handlePlayerSelection() {
-    let para1;
-    let para2; 
-    para1 = document.createElement("p");
-    div.appendChild(para1);
-    para1.textContent = ("You selected: " + playerSelection + "!");
-    para2 = document.createElement("p");
-    div.appendChild(para2);
-    para2.textContent = ("The computer selected: " + computerSelection + "!");
-}
-
-// Call getComputerChoice function
-getComputerChoice();
-
 // Resolve a round 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection = 'rock') {
+let resultRound;
+function playRound(playerSelection) {
+    if (playerSelection === "rock") {
         switch (computerSelection) {
-            case ('paper'):
-                resultMessage = "Defeat! Paper beats Rock.";
+            case "paper":
+                resultRound = "Defeat! Paper beats Rock.";
                 break;
-            case ('scissors'):
-                resultMessage = "Victory! Rock beats Scissors.";
+            case "scissors":
+                resultRound = "Victory! Rock beats Scissors.";
                 break;
-            default: resultMessage = "Tie!";
+            default: resultRound = "Tie!";
         }
-        return resultMessage;
-    } else if (playerSelection = 'paper') {
+    } else if (playerSelection === "paper") {
         switch (computerSelection) {
-            case ('rock'):
-                resultMessage = "Victory! Paper beats Rock.";
+            case "rock":
+                resultRound = "Victory! Paper beats Rock.";
                 break;
-            case ('scissors'):
-                resultMessage = "Defeat! Scissors beat Paper.";
+            case "scissors":
+                resultRound = "Defeat! Scissors beat Paper.";
                 break;
-            default: resultMessage = "Tie!";
+            default: resultRound = "Tie!";
         }
-        return resultMessage;
-    } else {
+    } else if (playerSelection === "scissors") {
         switch (computerSelection) {
-            case ('rock'):
-                resultMessage = "Defeat! Rock beats Scissors.";
+            case "rock":
+                resultRound = "Defeat! Rock beats Scissors.";
                 break;
-            case ('paper'):
-                resultMessage = "Victory! Scissors beat Paper.";
+            case "paper":
+                resultRound = "Victory! Scissors beat Paper.";
                 break;
-            default: resultMessage = "Tie!"; 
+            default: resultRound = "Tie!"; 
         }
-        return resultMessage;
     }
+    const para2 = document.createElement("p");
+    div.appendChild(para2);
+    para2.textContent = ("Round: " + resultRound);
 }
 
+let playerAnswer;
 let playerScore = 0;
 let computerScore = 0;
 
-// Write a NEW function called playGame(). Use the previous function inside of this one to play 
-// a five round game that keeps score and reports a winner or loser at the end.
-    
-    // Call playRound function
-playRound(playerSelection, computerSelection);
-console.log(resultMessage);
-if (resultMessage.charAt(0) === 'V') {
-    playerScore = ++playerScore;
-    console.log("Your score is " + playerScore + ".");
-    console.log("Computer's score is " + computerScore + ".");
-} else if (resultMessage.charAt(0) === 'D') {
-    computerScore = ++computerScore;
-    console.log("Your score is " + playerScore + ".");
-    console.log("Computer's score is " + computerScore + ".");
-} else {
-    console.log("Your score is " + playerScore + ".");
-    console.log("Computer's score is " + computerScore + ".");
-}                                         
-
+function gameScore() {
+    playRound();
+    const para3 = document.createElement("p");
+    div.appendChild(para3);
+    if (resultRound.charAt(0) === "V") {
+        playerScore = ++playerScore;
+    } else if (resultRound.charAt(0) === "D") {
+        computerScore = ++computerScore;
+    } else {
+    }   
+    para3.textContent += ("Your score is " + playerScore + ". ");
+    para3.textContent += ("Computer's score is " + computerScore + ".");
+}
+                        
 function finalResult() {
     if (playerScore > computerScore) {
-        console.log("Congratulations! You won the game!");
+        para3.textContent += ("Congratulations! You won the game!");
     } else if (playerScore < computerScore) {
-        console.log("You lost the game!");
+        para3.textContent += ("You lost the game!");
     } else {
-        console.log("It's a tie! Play again!");
+        para3.textContent += ("It's a tie! Play again!");
     }
 }
-
-finalResult();    
